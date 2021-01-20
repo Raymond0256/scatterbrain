c8da08bf27fe663414112692276e8f95e0390ed5
Raymond Lewis <raymond0256@gmail.com>
2021-01-20 15:23:32 -0600

    Add flake8-import-order.

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 nox.options.sessions = "lint", "tests"
 locations = "src", "tests", "noxfile.py"
 python_latest = "3.9"
-python_range = ["3.9", "3.8", "3.7", "3.6"]
+python_range = ["3.9", "3.8"]
 
 
 @nox.session(python=python_range)
@@ -20,7 +20,7 @@ def tests(session):
 def lint(session):
     """Linting for code in locations tuple."""
     args = session.posargs or locations
-    session.install("flake8", "flake8-black")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 

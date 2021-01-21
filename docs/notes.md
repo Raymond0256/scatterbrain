62620ddb7685502f75fc0cff4b6ce2812b384376
Raymond Lewis <raymond0256@gmail.com>
2021-01-21 17:58:25 -0600

    Add bugbear linting.

--- docs/notes.md
+++ docs/notes.md
@@ -8,3 +8,7 @@ I was a bit taken aback when I realized that GitHub already had a kanban board f
 but that is what I get for using GitLab all this time.  
 
 After a bit of investigation, the GitHub cards are incredibly rudementary and do not contain the features that I want, such as check lists and individual item due dates and flexible card layouts.  They are basically single box notes until you turn them into issues, which adds some but not all the features that I am looking for.
+
+## 2021-01-20 15:28:41  
+
+Still setting up the framework for the project.  Getting back into the groove of setting up a project after one, not contributing for a time, and two, using cookiecutter before that.  Luckily I was using such a well documented system that I can just follow along to get it all back into my working memory.  
--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,7 @@
 """Nox test file."""
+import tempfile
+
+
 import nox
 
 
@@ -6,6 +9,20 @@ nox.options.sessions = "lint", "tests"
 locations = "src", "tests", "noxfile.py"
 python_latest = "3.9"
 python_range = ["3.9", "3.8"]
+
+
+def install_with_constraints(session, *args, **kwargs):
+    """Install environment with restraints."""
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=python_range)
@@ -20,7 +37,7 @@ def tests(session):
 def lint(session):
     """Linting for code in locations tuple."""
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
     session.run("flake8", *args)
 
 
@@ -30,3 +47,11 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python=python_range)
+def mypy(session):
+    """Type checking."""
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)

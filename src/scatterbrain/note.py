eca32b406b3ceb36d5ddc5513baf7859396ab9df
Raymond Lewis <Raymond0256@gmail.com>
2021-02-04 22:43:56 -0600

    Add update_note.

--- src/scatterbrain/note.py
+++ src/scatterbrain/note.py
@@ -1,11 +1,19 @@
 """Note entry with metadata."""
+from datetime import datetime
+from os import getlogin
 
 
 class Note():
     """Note entry with metadata."""
 
-    def __init__(self, content: str, author: str, date: str) -> None:
+    def __init__(self, content: str, author: str) -> None:
         """Initializer or Note class."""
         self._content = content
-        self._author = author
-        self._date = date
+        self._author = getlogin()
+        self._date = datetime.now()
+
+    def update_note(self, updated_content) -> None:
+        """Update the content of a note."""
+        if self._content not in updated_content:
+            self._buffer = self._content
+        self._content = updated_content
